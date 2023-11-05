45e636a0d31bc8a27ba162378fca481c3971621e
Howard <digitalxess@gmail.com>
2023-11-05 18:31:43 -0500

    Update L5 - Matrix_chain_multi.cpp

--- CSCI_6632 - Algorithms/L5 - Dynamic Programming/Matrix_chain_multiplication/L5 - Matrix_chain_multi.cpp
+++ CSCI_6632 - Algorithms/L5 - Dynamic Programming/Matrix_chain_multiplication/L5 - Matrix_chain_multi.cpp
@@ -1,5 +1,11 @@
 // L5 - Matrix_chain_multi.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+//         1            2            3               4
+// 1,1=[  0, 0]  1,2=[ 24, 1]  1,3=[ 28, 1]  1,4=[ 58, 3]
+// 2,1=[      ]  2,2=[  0, 0]  2,3=[ 16, 2]  2,4=[ 36, 3]
+// 3,1=[      ]  3,2=[      ]  3,3=[  0, 0]  3,4=[ 40, 3]
+// 4,1=[      ]  4,2=[      ]  4,3=[      ]  4,4=[  0, 0]
+
 
 #include <iostream>
 #include <iomanip>
